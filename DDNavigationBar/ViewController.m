d5dc5bb3923bcd2f34b0c0023f8e487bc0fe17af
wangdada <472012723@qq.com>
2018-04-10 14:45:51 +0800

    diyici
    下拉 图片变大效果demo

--- DDNavigationBar/ViewController.m
+++ DDNavigationBar/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define imgHEIGHT 198
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property(nonatomic,strong)UITableView * tableView;
 
 @end
 
@@ -16,9 +20,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    float f = [[UIApplication sharedApplication] statusBarFrame].size.height;
+    _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, -f, self.view.frame.size.width, self.view.frame.size.height+f) style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [self.view addSubview:_tableView];
+    self.tableView.contentInset = UIEdgeInsetsMake(imgHEIGHT, 0, 0, 0);
+    UIImageView * imgview = [[UIImageView alloc]initWithFrame:CGRectMake(0, -imgHEIGHT, self.view.frame.size.width, imgHEIGHT)];
+    imgview.image = [UIImage imageNamed:@"Image"];
+    imgview.contentMode = UIViewContentModeScaleAspectFill;///如果不想放大 只想拉长。注释掉这句
+    imgview.tag = 101;
+    [_tableView addSubview:imgview];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    static NSString * cellid = @"cell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cellid];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellid];
+    }
+    return cell;
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGPoint point = scrollView.contentOffset;
+    if (point.y < -imgHEIGHT) {
+        CGRect rect = [self.tableView viewWithTag:101].frame;
+        rect.origin.y = point.y;
+        rect.size.height = -point.y;
+        
+        
+        [self.tableView viewWithTag:101].frame = rect;
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
